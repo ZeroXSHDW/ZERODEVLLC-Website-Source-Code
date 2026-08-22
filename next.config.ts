75836e742054efcf00215b19dccb1107b6669cf3
Admin <admin@Admins-MacBook-Pro.local>
2026-08-23 00:15:15 +0100

    Enforce cross-origin isolation headers

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const securityHeaders = [
   { key: 'X-Permitted-Cross-Domain-Policies', value: 'none' },
   { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
   { key: 'Cross-Origin-Resource-Policy', value: 'same-origin' },
+  { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
   { key: 'Origin-Agent-Cluster', value: '?1' },
   { key: 'X-DNS-Prefetch-Control', value: 'off' },
 ];
